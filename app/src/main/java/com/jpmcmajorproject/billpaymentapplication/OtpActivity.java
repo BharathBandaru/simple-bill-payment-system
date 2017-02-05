6de487c24d3c599838cd2ecdbf1a96b78e08693c
BharathBandaru <bharathbandaru@live.com>
2017-02-05 11:16:22 +0530

    start

---
+++ app/src/main/java/com/jpmcmajorproject/billpaymentapplication/OtpActivity.java
@@ -0,0 +1,16 @@
+package com.jpmcmajorproject.billpaymentapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Bharath on 05/02/17.
+ */
+
+public class OtpActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otp_activity);
+    }
+}
